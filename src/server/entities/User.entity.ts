46a29d86008fcbeb4dd45829b49c9799e62775a5
Unknown Haas <sharmasuhas450@gmail.com>
2026-03-01 18:06:20 +0530

    .

--- src/server/entities/User.entity.ts
+++ src/server/entities/User.entity.ts
@@ -20,7 +20,7 @@ export class User {
     @Column({ type: "varchar", nullable: true })
     resetPasswordToken?: string;
 
-    @Column({ type: "datetime", nullable: true })
+    @Column({ type: process.env.IS_LOCAL === 'true' ? "datetime" : "timestamp", nullable: true })
     resetPasswordExpires?: Date;
 
     @CreateDateColumn()
